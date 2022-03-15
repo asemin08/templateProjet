70a95a675997e8c51d043d1fc0e11d4df15ae628
asemin08 <semin.allan@gmail.com>
2022-03-15 21:57:10 +0100

    First commit

---
+++ src/main/java/eu/ensup/allan/projetAllan/ProjetAllanApplication.java
@@ -0,0 +1,13 @@
+package eu.ensup.allan.projetAllan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAllanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAllanApplication.class, args);
+	}
+
+}
